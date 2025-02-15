744036ff6791699a4b397bd19dd26478991c2201
Luke Partridge <lukej.partridge@icloud.com>
2025-02-15 09:54:15 +0000

    Continuing to build the JS foundations

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,3 +1,11 @@
+const coinsCount = document.getElementById('coins-count');
+const exchangeCount = document.getElementById('exchanges-count')
+const marketCap = document.getElementById('marketCap');
+const marketCapChangeElement = document.getElementById('marketCapChange');
+const volume = document.getElementById('volume');
+const dominance = document.getElementById('dominance');
+
+
 document.addEventListener("DOMContentLoaded", () =>{
     fetchGlobal();
 })
@@ -17,7 +25,33 @@ function getlocalStorageData(key){
     return parsedData.data;
 }
 
-function fetchGlobal(){
-
+function setLocalStorageData(key, data){
+    const storedData = {
+        timestamp: Date.now(),
+        data: data
+    }
+    localStorage.setItem(key, JSON.stringify(storedData));
 }
 
+function fetchGlobal(){
+    const localStorageKey = 'Global_Data';
+    const localData = getLocalStorageData(localStorageKey);
+
+    if(localData){
+        displayGlobalData(localData)
+    }else{
+        const options = {method: 'GET', headers: {accept: 'application/json'}};
+
+        fetch('https://api.coingecko.com/api/v3/global', options)
+            .then(response => response.json())
+            .then(data => {
+                const globalData = data.data;
+                displayGlobalData(data);
+                setLocalStorageData(localStorageKey, globalData);
+            })
+            .catch(error => {
+
+            })
+    }
+
+}
