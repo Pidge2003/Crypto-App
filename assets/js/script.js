360f627feef72bb1aee49ea2d024df9811f90284
Luke Partridge <lukej.partridge@icloud.com>
2025-02-16 12:25:22 +0000

    ...

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,4 +14,12 @@ function openTab(event, tabName){
 
     document.getElementById(tabName).style.display = "block";
     event.currentTarget.classList.add("active");
+
+    if (!tabDataLoaded[tabName]){
+        switch (tabName){
+            case: 'tab1':
+            fetchAndDisplay('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=true',['assets-list'], displayAssets,
+            'Crypto_Data');
+        }
+    }
 }
