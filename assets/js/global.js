d208fc7131fe15000da564c9d21c9ef10e668312
Luke Partridge <lukej.partridge@icloud.com>
2025-02-16 22:17:30 +0000

    Adding new functions to global JS for spinners

--- assets/js/global.js
+++ assets/js/global.js
@@ -84,3 +84,15 @@ function displayGlobalData(globalData){
 
     dominance.textContent = `BTC ${btcDominance} - ETH ${ethDominance}`;
 }
+
+function toggleSpinner(listId, spinnerId, show){
+    const listElement = document.getElementById(listId);
+    const spinnerElement =document.getElementById(spinnerId);
+
+    if(spinnerElement){
+        spinnerElement.style.display = show ? 'block' : 'none';
+    }
+    if(listElement){
+        listElement.style.display = show ? 'none' : 'block';
+    }
+}
