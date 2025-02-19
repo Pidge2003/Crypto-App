abe18087ff347479c99b8668fff196caba652a13
Luke Partridge <lukej.partridge@icloud.com>
2025-02-19 00:16:36 +0000

    Fixed issue with tabs no longer working

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,17 +15,17 @@ function openTab(event, tabName){
     document.getElementById(tabName).style.display = "block";
     event.currentTarget.classList.add("active");
 
-    if (!tabDataLoaded[tabName]){
+  if (!tabDataLoaded[tabName]){
         switch (tabName){
             case 'tab1':
                 fetchAndDisplay('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=true',['assets-list'], displayAssets, tabName,
                 'Crypto_Data');
                 break;
             case 'tab2':
-                fetchAndDisplay('https://api.coingecko/com/api/v3/exchanges', ['exchange-list'], displayExchanges, tabName, 'Exchanges_Data'):
+                fetchAndDisplay('https://api.coingecko/.com/api/v3/exchanges', ['exchange-list'], displayExchanges, tabName, 'Exchanges_Data');
                 break;
             case 'tab3':
-                fetchAndDisplay('https://api.cryptogecko.com/api/v3/coins/categories', [category-list], displayCategories, tabName, 'Categories_Data');
+                fetchAndDisplay('https://api.coingecko.com/api/v3/coins/categories', ['category-list'], displayCategories, tabName, 'Categories_Data');
                 break;
             case 'tab4':
                 fetchAndDisplay('https://api.coingecko.com/api/v3/companies/public_treasury/bitcoin', ['company-list'], displayCompanies, tabName, 'Companies_Data');
