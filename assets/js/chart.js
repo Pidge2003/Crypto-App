95c87dd2092ffa6486fd8f6c6dc82a000a3c9df4
Luke Partridge <lukej.partridge@icloud.com>
2025-02-23 21:58:49 +0000

    Adding trading view to charts page

---
+++ assets/js/chart.js
@@ -0,0 +1,27 @@
+function getThemeConfig(){
+    const root = getComputedStyle('document.documentElement');
+    const isDarkTheme = localStorage.getItem('theme') === 'light-theme' ? false : true;
+
+    const backgroundColor = root.getPropertyValue(isDarkTheme ? '--chart-dark-bg' : '--chart-light-bg').trim();
+    const gridColor = root.getPropertyValue(isDarkTheme ? '--chart-dark-border' : '--chart-light-border').trim();
+
+    return {
+        autosize: true,
+        symbol: "BINANCE:BTCUSDT",
+        interval: '4H',
+        timezone:'ETC/UTC',
+        theme: isDarkTheme ? 'dark' : 'light',
+        style: 1,
+        locale: "en",
+        container_id: "chart-widget",
+        backgroundColor: backgroundColor,
+        gridColor: gridColor,
+        hide_side_toolbar: false,
+        allow_symbol_change: true,
+        save_image: true,
+        details: true,
+        calendar: false,
+        support_host 'https://www.tradingview.com'
+
+    }
+}
