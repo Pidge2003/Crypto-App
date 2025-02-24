c38eab9ca441be3e2cd715a889366f46820f2971
Luke Partridge <lukej.partridge@icloud.com>
2025-02-24 23:43:39 +0000

    Intergrating trading view widgets

---
+++ assets/js/coin.js
@@ -0,0 +1,52 @@
+const widgetConfig1 = {
+    "symbol": "BINANCE:BTCUSDT",
+    "width": "100%",
+    "isTransparent": true,
+    "colorTheme": "dark",
+    "locale": "en"
+};
+
+const widgetConfig2 = {
+    "symbols": [
+        [
+            "BINANCE:BTCUSDT|1D"
+        ]
+    ],
+    "chartOnly": false,
+    "width": "100%",
+    "height": "100%",
+    "locale": "en",
+    "colorTheme": "dark",
+    "autosize": true,
+    "showVolume": false,
+    "showMA": false,
+    "hideDataRanges": false,
+    "hideMarketStatus": false,
+    "hideSymbolLogo": true,
+    "scalePosition":  "right",
+    "scaleMode": "Normal",
+    "fontFamiy": "-apple-system, BlinkMacSystemFont, Trebuchet MS, Roboto, Ubuntu, sans-serif",
+    "fontSize": "10",
+    "noTimeScale": false,
+    "valueTracking": "1",
+    "changeMode": "price-and-percent",
+    "chartType": "area",
+    "maLineColor": "#2962ff",
+    "maLineWidth": 1,
+    "maLength": 9,
+    "headerFontSize": "medium",
+    "backgroundColor": "rgba(14, 18, 24, 1)",
+    "gridLineColor": "rgba(76, 175, 80, 0.06)",
+    "lineWidth": 2,
+    "lineType": 0,
+    "dataRanges": [
+        "1D|15",
+        "1M|30",
+        "3M|60",
+        "12M|1D",
+        "60M|1W",
+        "all|1M"
+    ],
+    "dateFormat": "yyyy-mm-dd"
+
+}
